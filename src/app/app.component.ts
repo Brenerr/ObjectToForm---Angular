ae2fd71270e6e8217b2cad9222278518a42f7c43
Brenerr <brener820@gmail.com>
2022-06-07 14:27:56 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
     description: 'Terceira Automacao',
     automationType: 'save-update',
     workspace: '60033a8fa7a9285e35925032',
-    table: ['604977bafcf87e0013212c7c','604977bafcf87e0013212c7c'],
+    table: '604977bafcf87e0013212c7c',
     event: {
       eventType: 'isUpdateFor',
       config: {
